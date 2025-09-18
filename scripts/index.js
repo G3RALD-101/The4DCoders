48219878c26344567174475d2d04adfc13fa50a2
G3RALD-101 <gerald.villarias585@gmail.com>
2025-09-18 22:29:14 +0800

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("submitButton");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    button.addEventListener("click", (e) => {
+        e.preventDefault(); // prevents form from refreshing the page
+
+        const emailValue = email.value.trim();
+        const passwordValue = password.value.trim();
+
+        if (emailValue === "The4DCoders@gamedev.com.ph" && passwordValue === "The4DCoders_dev123") {
+            window.location.href = "../html/interface1.html";
+        } else {
+            alert("Invalid email or password!");
+        }
+    });
+});
